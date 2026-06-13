179dd852a0929bd6215e47527d00e633cf6b76c0
emrecanterzi <terzi.6112@gmail.com>
2026-06-13 10:49:28 +0300

    set HTTPS_PROXY via launchctl
    Electron apps like Discord ignore system proxy setttings. Using launchctl setenv propagates
    the variable to all newly launched processes system-wide.

--- internal/sysproxy/darwin.go
+++ internal/sysproxy/darwin.go
@@ -20,7 +20,16 @@ func (m *DarwinManager) SetProxy(addr, port string) error {
 	if err := exec.Command("networksetup", "-setwebproxy", m.service, addr, port).Run(); err != nil {
 		return err
 	}
-	return exec.Command("networksetup", "-setsecurewebproxy", m.service, addr, port).Run()
+
+	if err := exec.Command("networksetup", "-setsecurewebproxy", m.service, addr, port).Run(); err != nil {
+		return err
+	}
+
+	proxyURL := fmt.Sprintf("http://%s:%s", addr, port)
+	exec.Command("launchctl", "setenv", "HTTPS_PROXY", proxyURL).Run()
+	exec.Command("launchctl", "setenv", "HTTP_PROXY", proxyURL).Run()
+
+	return nil
 }
 
 func (m *DarwinManager) UnsetProxy() error {
@@ -28,5 +37,9 @@ func (m *DarwinManager) UnsetProxy() error {
 	if err := exec.Command("networksetup", "-setwebproxystate", m.service, "off").Run(); err != nil {
 		return err
 	}
+
+	exec.Command("launchctl", "unsetenv", "HTTPS_PROXY").Run()
+	exec.Command("launchctl", "unsetenv", "HTTP_PROXY").Run()
+
 	return exec.Command("networksetup", "-setsecurewebproxystate", m.service, "off").Run()
 }
